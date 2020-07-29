e034457d1f597afc9095b7b08c5be1c52d10ef1e
Leonti <prishelec@gmail.com>
2020-07-29 16:22:39 +1000

    Don't overwrite config

--- homeassistant/start.sh
+++ homeassistant/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp /tmp/configuration.yaml /config/configuration.yaml
+# Revert once lights are configured
+#cp /tmp/configuration.yaml /config/configuration.yaml
 cp /tmp/ui-lovelace.yaml /config/ui-lovelace.yaml
 
 python -m homeassistant --config /config
